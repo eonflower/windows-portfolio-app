f9ae515e06d4a15ccfe3805b23f4f9d71b8a4413
eonflower <eonflower.dev@gmail.com>
2024-06-21 20:39:28 -0700

    made modal more reusable

--- src/components/StartMenu.jsx
+++ src/components/StartMenu.jsx
@@ -1,25 +1,25 @@
-import { useState, useRef } from "react";
-import { IoMdArrowDropright } from "react-icons/io";
-import Modal from "./NewWindowModal";
-import { Rnd } from "react-rnd";
 
+import { Rnd } from "react-rnd";
+import { useState } from "react";
+import { IoMdArrowDropright } from "react-icons/io";
+import NewWindowModal from "./NewWindowModal";
 
 const StartMenu = () => {
   const [showStartMenu, setShowStartMenu] = useState(false);
-  const [openProgramModal, setOpenProgramModal] = useState(false);
-
-	const openModal = () => {
-		setOpenProgramModal(true);
-    setShowStartMenu(false);
-	};
-
-	const closeModal = () => {
-		setOpenProgramModal(false);
-	};
+  const [openModal, setOpenModal] = useState({ paint: false, minesweeper: false });
 
   const toggleStartMenu = () => {
     setShowStartMenu(!showStartMenu);
-  }
+  };
+
+  const openProgramModal = (program) => {
+    setOpenModal({ ...openModal, [program]: true });
+    setShowStartMenu(false);
+  };
+
+  const closeProgramModal = (program) => {
+    setOpenModal({ ...openModal, [program]: false });
+  };
 
   return (
     <div className="start-menu">
@@ -29,15 +29,19 @@ const StartMenu = () => {
         <ul>
           <li className="windows-update">Windows Update</li>
           <hr />
-          <li className="programs">Programs<span><IoMdArrowDropright size={18} />
-          </span>
+          <li className="programs">Programs<span><IoMdArrowDropright size={18} /></span>
             <div className="programs-container">
               <ul>
                 <li className="paint">
-                <button onClick={openModal}>
-                <img src="https://win98icons.alexmeub.com/icons/png/paint_file-0.png" alt="paint icon" />Paint</button></li>
+                  <button onClick={() => openProgramModal('paint')}>
+                    <img src="https://win98icons.alexmeub.com/icons/png/paint_file-0.png" alt="paint icon" />Paint
+                  </button>
+                </li>
                 <li className="minesweeper">
-                  <img src="https://win98icons.alexmeub.com/icons/png/game_mine_1-1.png" alt="minesweeper icon" />Minesweeper</li>
+                  <button onClick={() => openProgramModal('minesweeper')}>
+                    <img src="https://win98icons.alexmeub.com/icons/png/game_mine_1-1.png" alt="minesweeper icon" />Minesweeper
+                  </button>
+                </li>
               </ul>
             </div>
           </li>
@@ -53,19 +57,22 @@ const StartMenu = () => {
         dragHandleClassName="window-toolbar"
         size={{width: '100%'}}
         >
-			<Modal isOpen={openProgramModal} onClose={closeModal} title={'JSpaint.exe'}>
-      <iframe
-      id='paint'
-      src={'https://jspaint.app'}
-      width="100%"
-      style={{
-        overflow: "auto",
-      }}
-    />
-			</Modal>
-      </Rnd>
+      <NewWindowModal isOpen={openModal.paint} onClose={() => closeProgramModal('paint')} title="JSpaint.exe">
+        <iframe
+          id='paint'
+          src='https://jspaint.app'
+          width="100%"
+          height="80vh"
+          style={{ overflow: "auto" }}
+        />
+      </NewWindowModal></Rnd>
+      {/* <Rnd
+        dragHandleClassName="window-toolbar"
+        size={{width: '100%'}}>
+        <NewWindowModal isOpen={openModal.minesweeper} onClose={() => closeProgramModal('minesweeper')} title="Minesweeper.exe">
+      </NewWindowModal></Rnd> */}
     </div>
   );
-}
+};
 
 export default StartMenu;
