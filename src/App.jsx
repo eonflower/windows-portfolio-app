5c86604f64cc3ccea550eeb62e999cf63eb92c0b
eonflower <eonflower.dev@gmail.com>
2025-02-11 19:17:43 -0800

    removed initial splash with timer

--- src/App.jsx
+++ src/App.jsx
@@ -8,28 +8,6 @@ import Splash from './components/SplashScreen';
 
 function App() {
   const { pathname } = useLocation();
-  const [showSplashscreen, setShowSplashscreen] = useState(
-    () => !JSON.parse(localStorage.getItem("showedSplashscreen"))
-  );
-  useEffect(() => {
-    const showedSplashscreen = JSON.parse(localStorage.getItem('showedSplashscreen'));
-
-    if (!showedSplashscreen) {
-      // If it hasn't been shown, set a timer to hide it after 4000ms
-      const timer = setTimeout(() => {
-        setShowSplashscreen(false);
-        // Update localStorage to indicate that the splash screen has been shown
-        localStorage.setItem('showedSplashscreen', JSON.stringify(true));
-      }, 4000);
-
-      // Clear the timer if the component unmounts or is re-rendered before the timeout
-      return () => clearTimeout(timer);
-    } else {
-      // If the splash screen has been shown before, hide it immediately
-      setShowSplashscreen(false);
-    }
-  }, []);
-
   useLayoutEffect(() => {
     // Ensure that the scroll resets for the .window-content class
     const windowContent = document.querySelector('.window-content');
@@ -42,11 +20,10 @@ function App() {
 
   return (
     <div className="app">
-      {showSplashscreen ? <Splash />
-      :
       <>
       <NavWindow />
-      <Footer /></>}
+      <Footer />
+      </>
     </div>
   )
 }
